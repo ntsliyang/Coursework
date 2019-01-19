d8d9f0ea3f8c1b891c6e9f9ff6c08af22da00a3b
ntsliyang <yli546@usc.edu>
2019-01-19 09:29:53 -0800

    new changes

--- CS231n - Convolutional Neural Networks for Visual Recognition/Assignments/assignment1/cs231n/classifiers/linear_svm.py
+++ CS231n - Convolutional Neural Networks for Visual Recognition/Assignments/assignment1/cs231n/classifiers/linear_svm.py
@@ -24,23 +24,39 @@ def svm_loss_naive(W, X, y, reg):
   # compute the loss and the gradient
   num_classes = W.shape[1]
   num_train = X.shape[0]
+  num_pixels = X.shape[1]
   loss = 0.0
   for i in range(num_train):
+    # compute the score for ith training example 
+    # by multiplying each pixel with its corresponding weight 
+    # so that scores: A numpy array of shape (C, ) 
     scores = X[i].dot(W)
+    # as y[i] is the correct label, scores[y[i]] is the correct class's score
     correct_class_score = scores[y[i]]
     for j in range(num_classes):
       if j == y[i]:
         continue
+      # if j is not the correct class, we compute the margin according to formula
       margin = scores[j] - correct_class_score + 1 # note delta = 1
+      # we add loss += max(0, margin)
       if margin > 0:
         loss += margin
-
+        # reference from: https://cs231n.github.io/optimization-1/
+        dW[:,y[i]] -= X[i,:].T
+        dW[:,j] += X[i,:].T
+    
   # Right now the loss is a sum over all training examples, but we want it
   # to be an average instead so we divide by num_train.
   loss /= num_train
 
   # Add regularization to the loss.
   loss += reg * np.sum(W * W)
+
+  # divide the gradient of weights by the number of training examples 
+  dW /= num_train
+
+  # add the regularization term to the gradient of weights 
+  dW += reg*W
 
   #############################################################################
   # TODO:                                                                     #
@@ -63,17 +79,34 @@ def svm_loss_vectorized(W, X, y, reg):
   """
   loss = 0.0
   dW = np.zeros(W.shape) # initialize the gradient as zero
-
   #############################################################################
   # TODO:                                                                     #
   # Implement a vectorized version of the structured SVM loss, storing the    #
   # result in loss.                                                           #
   #############################################################################
-  pass
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
+  num_train = X.shape[0]
+  # scores: a numpy array of shape (N, C) containing scores for all training examples 
+  scores = X.dot(W) 
+    
+  # correct_class_score: a numpy array of shape (N, ) mapping from ith training example to ith correct training label's score 
+  # np.arange(num_train): array([0, 1, 2, ..., num_train])
+  # y: a numpy array of shape (N, ) containing correct training labels 
+  correct_class_score = scores[np.arange(num_train), y].reshape(-1, 1)
 
+  # calculate the margins' score according to formula 
+  margins = np.maximum(0, scores - correct_class_score + 1)
+    
+  # set correct training labels' margins to 0
+  margins[np.arange(num_train), y] = 0 
+  
+  # compute the average so we divide by num_train 
+  loss /= num_train
+
+  # add regularization term to the loss
+  loss += reg * np.sum(W * W)
 
   #############################################################################
   # TODO:                                                                     #
@@ -84,7 +117,19 @@ def svm_loss_vectorized(W, X, y, reg):
   # to reuse some of the intermediate values that you used to compute the     #
   # loss.                                                                     #
   #############################################################################
-  pass
+  
+  # 
+  margins[margins > 0] = 1
+    
+  margins[np.arange(num_train), y] = -np.sum(margins, axis=1)
+
+  dW = X.T.dot(margins)
+ 
+  # divide the number of training examples 
+  dW /= num_train
+    
+  # add regularization term to the gradient
+  dW += reg*W
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
---
+++ ITP-342/LiYangHW1/LiYangHW1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LiYangHW1
+//
+//  Created by apple on 2019/1/15.
+//  Copyright © 2019 ITP-342. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
